f20ca4408d0b0c08ffbfaac9c6499a53e8ef080e
7hangtime <dthrowe1@charlotte.edu>
2024-09-13 11:04:30 -0400

    CRUD

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
+    path('create-room/', views.createRoom, name="create-rr"),
+    path('update-room/<str:pk>', views.updateRoom, name="update-rr"),
+    path('delete-room/<str:pk>', views.deleteRoom, name="delete-rr"),
+
 ]
