e50bcc9a6cd2ce849c8931fb180d5b8d7f62bca7
7hangtime <dthrowe1@charlotte.edu>
2024-09-13 13:32:25 -0400

    User Login

--- base/urls.py
+++ base/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('create-room/', views.createRoom, name="create-rr"),
     path('update-room/<str:pk>', views.updateRoom, name="update-rr"),
     path('delete-room/<str:pk>', views.deleteRoom, name="delete-rr"),
-
+    path('login/', views.loginPage, name='login'),
 ]
